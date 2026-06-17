0b8016bcc47601582c5839a924ebe28f68b01f31
lucifertrj <jaintarun.abd17@gmail.com>
2026-06-18 02:58:32 +0530

    supporting files

---
+++ quiet_native.py
@@ -0,0 +1,19 @@
+import contextlib
+import os
+
+@contextlib.contextmanager
+def suppress_stderr():
+    stderr_fd = 2
+    saved_stderr_fd = os.dup(stderr_fd)
+    try:
+        with open(os.devnull, "w") as devnull:
+            os.dup2(devnull.fileno(), stderr_fd)
+            yield
+    finally:
+        os.dup2(saved_stderr_fd, stderr_fd)
+        os.close(saved_stderr_fd)
+
+def silence_stderr():
+    devnull_fd = os.open(os.devnull, os.O_WRONLY)
+    os.dup2(devnull_fd, 2)
+    os.close(devnull_fd)
